72dd2c510ae84b274f00df46b1547f67fa25e43b
Sultanxda <sultanxda@gmail.com>
2016-08-06 10:57:04 -0700

    input: synaptics: s3320: Silence log spam
    Change-Id: Iee3648948b80b798c924920ce0c31bf380536aac
    input: synaptics: s3320: Remove 'all finger up' log spam
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1361,7 +1361,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 }
 #endif
 /***************end****************/
-static char prlog_count = 0;
+
 #ifdef REPORT_2D_PRESSURE
 static unsigned char pres_value = 1;
 #endif
@@ -1560,8 +1560,6 @@ void int_touch(void)
 
 	if (finger_num == 0/* && last_status && (check_key <= 1)*/)
 	{
-		if (3 == (++prlog_count % 6))
-			TPD_ERR("all finger up\n");
 		input_report_key(ts->input_dev, BTN_TOOL_FINGER, 0);
 #ifndef TYPE_B_PROTOCOL
 		input_mt_sync(ts->input_dev);
