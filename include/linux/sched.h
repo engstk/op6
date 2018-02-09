263680d7fd823458b4d48bfe7978acc649b126ff
joshuous <joshuous@gmail.com>
2018-02-09 22:35:29 +0800

    sched/tune: Add initial support for Dynamic SchedTune Boost
    Provide functions to activate and reset SchedTune boost:
    
    int do_stune_boost(char *st_name, int boost);
    int reset_stune_boost(char *st_name);
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -4038,6 +4038,11 @@ void cpufreq_add_update_util_hook(int cpu, struct update_util_data *data,
 void cpufreq_remove_update_util_hook(int cpu);
 #endif /* CONFIG_CPU_FREQ */
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+int do_stune_boost(char *st_name, int boost);
+int reset_stune_boost(char *st_name);
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 extern DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 
 #endif
