f0c564782ee858fa63121a5dfd6d5e451bcf05d9
engstk <eng.stk@sapo.pt>
2018-06-08 11:22:01 +0100

    input: synaptics: s3320: add haptic feedback control sysfs
    thanks to boeffla for the original vibrator hooks and flar for OP6 adapt
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -64,6 +64,9 @@
 #include "synaptics_baseline.h"
 #include "synaptics_dsx_core.h"
 #include <linux/oneplus/boot_mode.h>
+
+#include <linux/moduleparam.h>
+
 /*------------------------------------------------Global Define--------------------------------------------*/
 
 #define TP_UNKNOWN 0
@@ -193,6 +196,11 @@ int Single_gesture = 0; //"(SingleTap)"
 int Enable_gesture =0;
 static int gesture_switch = 0;
 #endif
+
+bool haptic_feedback_disable = false;
+module_param(haptic_feedback_disable, bool, 0644);
+
+void qpnp_hap_ignore_next_request(void);
 
 /*********************for Debug LOG switch*******************/
 #define TPD_ERR(a, arg...)  pr_debug(TPD_DEVICE ": " a, ##arg)
@@ -1554,6 +1562,9 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 		input_sync(ts->input_dev);
 		input_report_key(ts->input_dev, keyCode, 0);
 		input_sync(ts->input_dev);
+
+		if (haptic_feedback_disable)
+			qpnp_hap_ignore_next_request();
 	}else{
 		mutex_lock(&ts->mutex);
 		ret = i2c_smbus_read_i2c_block_data( ts->client, F12_2D_CTRL20, 3, &(reportbuf[0x0]) );
