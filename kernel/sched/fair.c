37778dc683c26d07d8520c9da97c2cd19725f1c6
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2018-06-19 14:19:29 -0700

    sched: improve trace prints
    Add the target and backup cpu in find_best_target trace print.
    During placement print more information about the cpu before
    skipping it. This helps in understanding why the cpu was not
    considered for placement.
    
    Change-Id: Ifc4de6b1d7495d2d701a92f465f4c81e86f468ea
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    [clingutla@codeaurora.org: Resolved merge conflicts]
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6983,6 +6983,7 @@ retry:
 
 			cpumask_clear_cpu(i, &search_cpus);
 
+			trace_sched_cpu_util(i);
 			if (!cpu_online(i) || cpu_isolated(i))
 				continue;
 
@@ -6993,8 +6994,6 @@ retry:
 
 			if (walt_cpu_high_irqload(i) || is_reserved(i))
 				continue;
-
-			trace_sched_cpu_util(i);
 
 			/*
 			 * p's blocked utilization is still accounted for on prev_cpu
@@ -7066,7 +7065,8 @@ retry:
 					trace_sched_find_best_target(p,
 							prefer_idle, min_util,
 							cpu, best_idle_cpu,
-							best_active_cpu, i);
+							best_active_cpu,
+							i, -1);
 
 					return i;
 				}
@@ -7287,7 +7287,7 @@ retry:
 
 	trace_sched_find_best_target(p, prefer_idle, min_util, cpu,
 				     best_idle_cpu, best_active_cpu,
-				     target_cpu);
+				     target_cpu, *backup_cpu);
 
 	schedstat_inc(p->se.statistics.nr_wakeups_fbt_count);
 	schedstat_inc(this_rq()->eas_stats.fbt_count);
