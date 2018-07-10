f03980d655aa28c5effbc5490d0e661b0f31d7cb
Erik Müller <pappschlumpf>
2018-07-10 21:45:07 +0200

    drivers: add param_read_write stub
    (squash and adjust)
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

---
+++ drivers/param_read_write/param_read_write.c
@@ -0,0 +1,12 @@
+/*
+ * drivers/param_read_write/param_read_write.c 
+ * Fake param_read_write Avengers theme
+ * Made by pappschlumpf (Erik Müller)
+ */
+
+#include <linux/input.h>
+#include <linux/moduleparam.h>
+
+static unsigned int cust_flag = 3;
+
+module_param(cust_flag, uint, 0644);
