cd9844ce2207ccb9b9e7b044eadcd7ddaad47338
zhanglin <jennifer.zhang@oneplus.net>
2018-10-25 18:02:15 +0800

    fix cannot steadliy record 30fps video on HFR 480 fps mode
    Change-Id: I00793595727557af15e7b250ba1e3605493a6c30
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/media/platform/msm/vidc/msm_vidc_clocks.c
+++ drivers/media/platform/msm/vidc/msm_vidc_clocks.c
@@ -82,6 +82,25 @@ static inline int msm_vidc_get_mbs_per_frame(struct msm_vidc_inst *inst)
 	return NUM_MBS_PER_FRAME(height, width);
 }
 
+static int msm_vidc_get_fps(struct msm_vidc_inst *inst)
+{
+	int fps = 0;
+
+	if (!inst) {
+		dprintk(VIDC_ERR, "%s: invalid params\n", __func__);
+		goto exit;
+	}
+
+	if ((inst->clk_data.operating_rate >> 16) > inst->prop.fps)
+		fps = (inst->clk_data.operating_rate >> 16) ?
+			(inst->clk_data.operating_rate >> 16) : 1;
+	else
+		fps = inst->prop.fps;
+
+exit:
+	return fps;
+}
+
 void update_recon_stats(struct msm_vidc_inst *inst,
 	struct recon_stats_type *recon_stats)
 {
@@ -253,12 +272,7 @@ int msm_comm_vote_bus(struct msm_vidc_core *core)
 			msm_comm_g_ctrl_for_id(inst,
 				V4L2_CID_MPEG_VIDC_VIDEO_NUM_B_FRAMES) != 0;
 
-		if (inst->clk_data.operating_rate)
-			vote_data[i].fps =
-				(inst->clk_data.operating_rate >> 16) ?
-				inst->clk_data.operating_rate >> 16 : 1;
-		else
-			vote_data[i].fps = inst->prop.fps;
+		vote_data[i].fps = msm_vidc_get_fps(inst);
 
 		vote_data[i].power_mode = 0;
 		if (!msm_vidc_clock_scaling || is_turbo ||
@@ -607,7 +621,7 @@ static unsigned long msm_vidc_calc_freq(struct msm_vidc_inst *inst,
 	struct msm_vidc_core *core = NULL;
 	int i = 0;
 	struct allowed_clock_rates_table *allowed_clks_tbl = NULL;
-	u64 rate = 0;
+	u64 rate = 0, fps;
 	struct clock_data *dcvs = NULL;
 	u32 operating_rate, vsp_factor_num = 10, vsp_factor_den = 7;
 
@@ -616,6 +630,8 @@ static unsigned long msm_vidc_calc_freq(struct msm_vidc_inst *inst,
 
 	mbs_per_second = msm_comm_get_inst_load_per_core(inst,
 		LOAD_CALC_NO_QUIRKS);
+
+	fps = msm_vidc_get_fps(inst);
 
 	/*
 	 * Calculate vpp, vsp cycles separately for encoder and decoder.
@@ -650,7 +666,7 @@ static unsigned long msm_vidc_calc_freq(struct msm_vidc_inst *inst,
 
 		vsp_cycles = mbs_per_second * inst->clk_data.entry->vsp_cycles;
 		/* 10 / 7 is overhead factor */
-		vsp_cycles += ((inst->prop.fps * filled_len * 8) * 10) / 7;
+		vsp_cycles += ((fps * filled_len * 8) * 10) / 7;
 
 	} else {
 		dprintk(VIDC_ERR, "Unknown session type = %s\n", __func__);
@@ -1116,7 +1132,7 @@ static inline int msm_vidc_power_save_mode_enable(struct msm_vidc_inst *inst,
 	}
 	mbs_per_frame = msm_vidc_get_mbs_per_frame(inst);
 	if (mbs_per_frame > inst->core->resources.max_hq_mbs_per_frame ||
-		inst->prop.fps > inst->core->resources.max_hq_fps) {
+		msm_vidc_get_fps(inst) > inst->core->resources.max_hq_fps) {
 		enable = true;
 	}
 
