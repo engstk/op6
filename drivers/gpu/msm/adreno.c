e4926499adc08923d1118b1b0f3a35d5bd3ccb28
mady51 <root@instance-1.c.thematic-gift-200703.internal>
2019-01-13 14:50:29 +0530

    Disable Adreno Driver Logging
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -58,7 +58,7 @@ MODULE_PARM_DESC(swfdetect, "Enable soft fault detection");
 #define DRIVER_VERSION_MAJOR   3
 #define DRIVER_VERSION_MINOR   1
 
-#define KGSL_LOG_LEVEL_DEFAULT 3
+#define KGSL_LOG_LEVEL_DEFAULT 0
 
 static void adreno_input_work(struct work_struct *work);
 static unsigned int counter_delta(struct kgsl_device *device,
