07e098955c96673e55fd4ec0cdbaed0346bb0941
flar2 <asegaert@gmail.com>
2019-05-22 01:13:44 -0400

    Allow stock modules to load
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- kernel/module.c
+++ kernel/module.c
@@ -2986,7 +2986,7 @@ static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 		modmagic = NULL;
 
 	/* This is allowed: modprobe --force will invalidate it. */
-	if (!modmagic) {
+	/*if (!modmagic) {
 		err = try_to_force_load(mod, "bad vermagic");
 		if (err)
 			return err;
@@ -2994,7 +2994,7 @@ static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 		pr_err("%s: version magic '%s' should be '%s'\n",
 		       mod->name, modmagic, vermagic);
 		return -ENOEXEC;
-	}
+	}*/
 
 	if (!get_modinfo(info, "intree")) {
 		if (!test_taint(TAINT_OOT_MODULE))
