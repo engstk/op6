1de0762e3bed0cb87ce0ef6e15647b19b289f476
Tyler Nijmeh <tylernij@gmail.com>
2019-07-31 02:14:48 -0700

    usb_bam: Disable logging
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/platform/msm/usb_bam.c
+++ drivers/platform/msm/usb_bam.c
@@ -44,7 +44,7 @@
 #define ARRAY_INDEX_FROM_ADDR(base, addr) ((addr) - (base))
 
 #define ENABLE_EVENT_LOG 1
-static unsigned int enable_event_log = ENABLE_EVENT_LOG;
+static unsigned int enable_event_log;
 module_param(enable_event_log, uint, 0644);
 MODULE_PARM_DESC(enable_event_log, "enable event logging in debug buffer");
 
