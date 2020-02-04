c08662933ef8054f8966248ffd65b17fec936958
engstk <eng.stk@sapo.pt>
2020-02-04 00:42:44 +0000

    Revert "initramfs: add skip_override parameter"
    This reverts commit 8430eaac330123e2b62033d2ce88ab48befcc8b9.
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- init/initramfs.c
+++ init/initramfs.c
@@ -607,16 +607,6 @@ static void __init clean_rootfs(void)
 }
 #endif
 
-static int __initdata skip_override = 0;
-
-static int __init skip_override_param(char *str)
-{
-	skip_override = 1;
-	return 1;
-}
-__setup("skip_override", skip_override_param);
-
-
 static int __initdata do_skip_initramfs;
 
 static int __init skip_initramfs_param(char *str)
@@ -632,7 +622,7 @@ static int __init populate_rootfs(void)
 {
 	char *err;
 
-	if (!skip_override && do_skip_initramfs) {
+	if (do_skip_initramfs) {
 		if (initrd_start)
 			free_initrd();
 		return default_rootfs();
