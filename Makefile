engstk/op7

kbuild: Increase automatic inline instruction limit to 1000 for gcc-10
GCC 10 updated its interprocedural optimizer's logic to have it make
more conservative inlining decisions, resulting in worse syscall and
hackbench performance compared to GCC 9. Although the max-inline-insns-
auto parameter's value was not altered, increasing it from the -O3
default of 30 to 1000 instructions yields improved performance with LTO,
surpassing GCC 9.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

Signed-off-by: engstk <eng.stk@sapo.pt>

Makefile
@@ -432,7 +432,8 @@ KBUILD_CFLAGS   := -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
 		   -Werror-implicit-function-declaration \
 		   -Wno-format-security \
 		   -std=gnu89 \
-		   -mcpu=cortex-a76.cortex-a55 -mtune=cortex-a76.cortex-a55 -fdiagnostics-color=always -ftree-vectorize -pipe \
+		   -mcpu=cortex-a76.cortex-a55 -mtune=cortex-a76.cortex-a55 \
+		   -fdiagnostics-color=always -ftree-vectorize --param=max-inline-insns-auto=1000 -pipe \
 		   -Wno-maybe-uninitialized -Wno-unused-variable -Wno-unused-function -Wno-unused-label \
 		   -Wno-memset-transposed-args -Wno-bool-compare -Wno-logical-not-parentheses -Wno-discarded-array-qualifiers \
 		   -Wno-unused-const-variable -Wno-array-bounds -Wno-incompatible-pointer-types \

engstk/op6

Makefile
@@ -400,7 +400,8 @@ KBUILD_CFLAGS   := -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
 		   -Werror-implicit-function-declaration \
 		   -Wno-format-security \
 		   -std=gnu89 \
-		   -mcpu=cortex-a75.cortex-a55 -mtune=cortex-a75.cortex-a55 -fdiagnostics-color=always -ftree-vectorize -pipe \
+		   -mcpu=cortex-a75.cortex-a55 -mtune=cortex-a75.cortex-a55 \
+		   -fdiagnostics-color=always -ftree-vectorize --param=max-inline-insns-auto=1000 -pipe \
 		   -Wno-maybe-uninitialized -Wno-unused-variable -Wno-unused-function -Wno-unused-label \
 		   -Wno-memset-transposed-args -Wno-bool-compare -Wno-logical-not-parentheses -Wno-discarded-array-qualifiers \
 		   -Wno-unused-const-variable -Wno-array-bounds -Wno-incompatible-pointer-types \