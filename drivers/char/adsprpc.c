e916ec0de71511ffe3912f3f6d75dfe96a22c0bc
Bhanu Kashyap <bhanu.kashyap@oneplus.com>
2021-05-02 16:09:15 +0530

    Synchronize codes for Oneplus 6T OxygenOS 10.3.11/ Oneplus 6 OxygenOS 10.3.11
    [CVE-2021-1927] {msm:ADSPRPC :Fix to avoid Use after free in fastrpc_internal_munmap}
    
    Change-Id: I5dc004712c27fbcc0c8f32e81c25e0e0147eece2
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -2706,13 +2706,15 @@ static int fastrpc_internal_munmap(struct fastrpc_file *fl,
 	mutex_unlock(&fl->fl_map_mutex);
 	if (err)
 		goto bail;
-	VERIFY(err, !fastrpc_munmap_on_dsp(fl, map->raddr,
-				map->phys, map->size, map->flags));
-	if (err)
-		goto bail;
-	mutex_lock(&fl->fl_map_mutex);
-	fastrpc_mmap_free(map, 0);
-	mutex_unlock(&fl->fl_map_mutex);
+	if (map) {
+		VERIFY(err, !fastrpc_munmap_on_dsp(fl, map->raddr,
+					map->phys, map->size, map->flags));
+		if (err)
+			goto bail;
+		mutex_lock(&fl->fl_map_mutex);
+		fastrpc_mmap_free(map, 0);
+		mutex_unlock(&fl->fl_map_mutex);
+	}
 bail:
 	if (err && map) {
 		mutex_lock(&fl->fl_map_mutex);
